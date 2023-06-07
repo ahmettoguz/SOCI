e70ae08bc549cec4b030d6377f9c16ea39c4ebb5
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-06-07 17:40:21 +0300

    Feat: Fragment swiper is in progress.

--- app/src/main/java/com/soci/soci/MainActivity.java
+++ app/src/main/java/com/soci/soci/MainActivity.java
@@ -2,24 +2,16 @@ package com.soci.soci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Toast;
 
-import com.soci.soci.databinding.ActivityMainBinding;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.widget.ViewPager2;
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
-
 import com.google.android.material.tabs.TabLayout;
+import com.soci.soci.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,61 +41,30 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
 
-        binding.registActBttn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = null;
-
-                intent = new Intent(MainActivity.this, InformationActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = null;
-
-                intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        binding.mainBtnAddEvent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Add clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-        // related with swipe
-        viewPager = findViewById(R.id.main_Vp_ViewPager);
-        tabLayout = findViewById(R.id.main_Tl_TabLayout);
-
+        // swiper related
         List<Fragment> fragmentList = new ArrayList<>();
         fragmentList.add(new UserEventFragment());
         fragmentList.add(new EventsFragment());
         fragmentList.add(new UserInformationFragment());
-
         pagerAdapter = new PagerAdapterWrapper(this, fragmentList);
-        viewPager.setAdapter(pagerAdapter);
+        binding.mainVpViewPager.setAdapter(pagerAdapter);
+        binding.mainTlTabLayout.addTab(binding.mainTlTabLayout.newTab().setText("Page 1"));
+        binding.mainTlTabLayout.addTab(binding.mainTlTabLayout.newTab().setText("Page 2"));
+        binding.mainTlTabLayout.addTab(binding.mainTlTabLayout.newTab().setText("Page 3"));
 
-        tabLayout.addTab(tabLayout.newTab().setText("Page 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Page 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Page 3"));
-
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+        // swiper related
+        binding.mainVpViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
-                tabLayout.selectTab(tabLayout.getTabAt(position));
+                binding.mainTlTabLayout.selectTab(binding.mainTlTabLayout.getTabAt(position));
             }
         });
 
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        // swiper related
+        binding.mainTlTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+                binding.mainVpViewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
@@ -116,5 +77,8 @@ public class MainActivity extends AppCompatActivity {
                 // Do nothing
             }
         });
+
+        // swiper related start from events fragment
+        binding.mainVpViewPager.setCurrentItem(1,false);
     }
 }
