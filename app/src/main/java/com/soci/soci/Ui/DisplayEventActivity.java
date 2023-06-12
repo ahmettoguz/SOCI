73d9a478ec2f8e400c203ad3f04573f448a1e689
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-06-12 03:26:49 +0300

    Feat: Display event page desing is finished.

--- app/src/main/java/com/soci/soci/Ui/DisplayEventActivity.java
+++ app/src/main/java/com/soci/soci/Ui/DisplayEventActivity.java
@@ -46,8 +46,19 @@ public class DisplayEventActivity extends AppCompatActivity {
         Intent receivedIntent = getIntent();
         current_Person = MainSys.getPersonById(receivedIntent.getIntExtra("person_Id", -1));
         current_Event = MainSys.getEventFromId(receivedIntent.getIntExtra("event_Id", -1));
-        MainSys.msg(this, current_Event.getName());
 
+        // set components
+        binding.eventTVName.setText(current_Event.getName());
+        binding.eventTVDescription.setText(binding.eventTVDescription.getText() + current_Event.getDescription());
+        binding.eventTVStartDate.setText(current_Event.getStart_Date());
+        binding.eventTVEndDate.setText(current_Event.getEnd_Date());
+        binding.eventTVPlace.setText(current_Event.getLocation());
 
+        int participated_People_Count = 0;
+        binding.eventTVQuota.setText(binding.eventTVQuota.getText().toString() + participated_People_Count + "/" + current_Event.getMax_Participant());
+
+        String imgName = MainSys.getImgNameFromCategory(current_Event.getCategory());
+        int imgId = MainSys.convertImageNameToId(this, imgName);
+        binding.eventIvCategory.setImageResource(imgId);
     }
 }
