3c7e3479d260fc8909ce69f9c2518c055e737326
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-06-14 15:18:20 +0300

    Feat: Add activity will be performed

---
+++ app/src/main/java/com/soci/soci/Ui/Event_Add_Activity.java
@@ -0,0 +1,16 @@
+package com.soci.soci.Ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.soci.soci.R;
+
+public class Event_Add_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_add);
+    }
+}
