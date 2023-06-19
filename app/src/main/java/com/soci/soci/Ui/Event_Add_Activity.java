f3439c5e4e988c98c280521e078ea2267f9b0d39
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-06-19 08:53:09 +0300

    Feat: Add page in progress. Dynamic operation in progress.

--- app/src/main/java/com/soci/soci/Ui/Event_Add_Activity.java
+++ app/src/main/java/com/soci/soci/Ui/Event_Add_Activity.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.soci.soci.Business.MainSys;
 import com.soci.soci.Model.Person;
@@ -18,6 +21,8 @@ import com.soci.soci.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Event_Add_Activity extends AppCompatActivity {
     ActivityEventAddBinding binding;
@@ -55,15 +60,84 @@ public class Event_Add_Activity extends AppCompatActivity {
         }
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, categoryList);
         binding.addEventSpCategories.setAdapter(adapter);
+        String imgName = "category_all";
+        int img_ID = MainSys.convertImageNameToId(Event_Add_Activity.this, imgName);
+        binding.addEventIvCategoryImg.setImageResource(img_ID);
+
+        // spinner event
+        binding.addEventSpCategories.setSelection(0, false);
+        binding.addEventSpCategories.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = ((TextView) view).getText().toString();
+                if (!selectedItem.equalsIgnoreCase("Select category")) {
+                    Log.d("ahmet", selectedItem);
+                    String imgName = MainSys.getImgNameFromCategory(selectedItem);
+                    int img_ID = MainSys.convertImageNameToId(Event_Add_Activity.this, imgName);
+                    binding.addEventIvCategoryImg.setImageResource(img_ID);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         // automatically fill the form
-        fillForm();
+        //fillForm();
 
-        MainSys.msg(this, current_Person.getName());
+        // cancel button event
+        binding.eventBtnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        binding.eventBtnApprove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, String> result = checkErrors();
+
+                if (result.get("success").equalsIgnoreCase("true")) {
+
+                } else {
+                    MainSys.msg(Event_Add_Activity.this, result.get("message"));
+                }
+            }
+        });
     }
 
     private void fillForm() {
+        binding.addEventSpCategories.setSelection(4, false);
+        binding.addEventEtName.setText("Office Tour");
+        binding.addEventEtDescription.setText("Let's meet up and visit the office of Meteksan Savunma.");
+        binding.addEventEtPlace.setText("Bilkent/Cyberpark");
+        binding.addEventEtQuota.setText("4");
+        binding.addEventEtStartDate.setText("19.06.2023-10.00");
+        binding.addEventEtEndDate.setText("19.06.2023-12.00");
+    }
 
+    private Map<String, String> checkErrors() {
+        Map<String, String> result = new HashMap<>();
+        result.put("success", "false");
 
+        if (binding.addEventSpCategories.getSelectedItem().toString().equalsIgnoreCase("Select category"))
+            result.put("message", "Select category!");
+        else if (binding.addEventEtName.getText().toString().length() == 0)
+            result.put("message", "Event name is empty!");
+        else if (binding.addEventEtDescription.getText().toString().length() == 0)
+            result.put("message", "Description is empty!");
+        else if (binding.addEventEtPlace.getText().toString().length() == 0)
+            result.put("message", "Place is empty!");
+        else if (binding.addEventEtStartDate.getText().toString().length() == 0)
+            result.put("message", "Start date is empty!");
+        else if (binding.addEventEtEndDate.getText().toString().length() == 0)
+            result.put("message", "End date is empty!");
+        else
+            result.put("success", "true");
+
+        return result;
     }
 }
